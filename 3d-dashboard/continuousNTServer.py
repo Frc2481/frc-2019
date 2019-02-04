b5a298995c0481da81c966e766806b42a408ca8a
Jdeck0X <eckhardt.joshd@gmail.com>
2019-02-03 22:11:14 -0600

    3D Dashboard initial commit

---
+++ 3d-dashboard/continuousNTServer.py
@@ -0,0 +1,13 @@
+from networktables import NetworkTables
+import time
+import math
+
+NetworkTables.initialize()
+sd = NetworkTables.getTable("SmartDashboard")
+
+i = 0
+while True:
+    sd.putNumber("ElevatorPosition", 3*(math.sin(i*0.25)+1))
+    i = i + 1
+    print i
+    time.sleep(0.1)
