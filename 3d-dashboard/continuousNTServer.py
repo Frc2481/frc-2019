b3f30aa1a1bdc7520a116ca66503246ba6dbc9ef
Jdeck0X <eckhardt.joshd@gmail.com>
2019-02-13 00:36:12 -0600

    Split elevator model into two - elevator assembly and hatch slide.  Animated both models independently.

--- 3d-dashboard/continuousNTServer.py
+++ 3d-dashboard/continuousNTServer.py
@@ -8,6 +8,7 @@ sd = NetworkTables.getTable("SmartDashboard")
 i = 0
 while True:
     sd.putNumber("ElevatorPosition", 3*(math.sin(i*0.25)+1))
+    sd.putNumber("HatchSlidePosition", 10*(math.sin(i*0.25)+1))
     i = i + 1
     print i
     time.sleep(0.1)
