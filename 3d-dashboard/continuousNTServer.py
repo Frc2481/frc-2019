9d516c435d8607c416045b7485610bcede8c84e4
Jdeck0X <eckhardt.joshd@gmail.com>
2019-02-13 19:44:16 -0600

    Split elevator into three parts and animated each one vertically.

--- 3d-dashboard/continuousNTServer.py
+++ 3d-dashboard/continuousNTServer.py
@@ -7,7 +7,8 @@ sd = NetworkTables.getTable("SmartDashboard")
 
 i = 0
 while True:
-    sd.putNumber("ElevatorPosition", 3*(math.sin(i*0.25)+1))
+    sd.putNumber("ElevatorSlidePosition", 3*(math.sin(i*0.25)+1))
+    sd.putNumber("ElevatorPosition", 20*(math.sin(i*0.25)+1))
     sd.putNumber("HatchSlidePosition", 10*(math.sin(i*0.25)+1))
     i = i + 1
     print i
