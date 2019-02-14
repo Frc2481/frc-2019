2dba5c1cce433415a33308a57b853c53f330e4b5
Jdeck0X <eckhardt.joshd@gmail.com>
2019-02-13 22:35:06 -0600

    Further reduced polygon count of models to improve load times. Added cargo and hatch panels.

--- 3d-dashboard/continuousNTServer.py
+++ 3d-dashboard/continuousNTServer.py
@@ -7,9 +7,14 @@ sd = NetworkTables.getTable("SmartDashboard")
 
 i = 0
 while True:
-    sd.putNumber("ElevatorSlidePosition", 3*(math.sin(i*0.25)+1))
-    sd.putNumber("ElevatorPosition", 20*(math.sin(i*0.25)+1))
-    sd.putNumber("HatchSlidePosition", 10*(math.sin(i*0.25)+1))
+    sd.putNumber("ElevatorSlidePosition", 3*(math.sin(i*0.125)+1))
+    sd.putNumber("ElevatorPosition", 20*(math.sin(i*0.125)+1))
+    sd.putNumber("HatchSlidePosition", 10*(math.sin(i*0.125)+1))
+
+    cargoOrHatch = (i%300)/100 #Use integer division to our advantage.  0 - nothing, 1 - cargo, 2 - hatch
+    sd.putBoolean("HasCargo", cargoOrHatch == 1)
+    sd.putBoolean("HasHatch", cargoOrHatch == 2)
+    
     i = i + 1
     print i
-    time.sleep(0.1)
+    time.sleep(0.05)
