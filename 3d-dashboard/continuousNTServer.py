9fb3ffd4591750425629c88514016f6d663a5047
Jdeck0X <eckhardt.joshd@gmail.com>
2019-03-11 23:23:24 -0500

    Changes from 3/9/2019.  Updated models, corrected animations, reading actual values sent by robot.  Tested with robot code.  Added camera stream.

--- 3d-dashboard/continuousNTServer.py
+++ 3d-dashboard/continuousNTServer.py
@@ -7,16 +7,20 @@ sd = NetworkTables.getTable("SmartDashboard")
 
 i = 0
 while True:
-    sd.putNumber("ElevatorSlidePosition", 3*(math.sin(i*0.125)+1))
-    sd.putNumber("ElevatorPosition", 20*(math.sin(i*0.125)+1))
-    sd.putNumber("HatchSlidePosition", 10*(math.sin(i*0.125)+1))
-    sd.putNumber("IntakePosition", 7*(math.sin(i*0.125)+1))
+    sd.putNumber("ElevatorPos", 20*(math.sin(i*0.125)+1))
+    sd.putNumber("HatchSlidePos", 8.5*(math.sin(i*0.125)))
+    sd.putNumber("IntakePosition", 8.5*(math.sin(i*0.125)+1))
     sd.putNumber("LineStrip1Position", 10*math.sin(i*0.125))
     sd.putNumber("LineStrip2Position", 10*math.sin(i*0.125+math.pi))
 
     cargoOrHatch = (i%300)/100 #Use integer division to our advantage.  0 - nothing, 1 - cargo, 2 - hatch
     sd.putBoolean("HasCargo", cargoOrHatch == 1)
     sd.putBoolean("HasHatch", cargoOrHatch == 2)
+
+    sd.putNumber("FL steer encoder pos", (i%360)-180)
+    sd.putNumber("FR steer encoder pos", (i%360)-180)
+    sd.putNumber("BL steer encoder pos", (i%360)-180)
+    sd.putNumber("BR steer encoder pos", (i%360)-180)
     
     i = i + 1
     print i
